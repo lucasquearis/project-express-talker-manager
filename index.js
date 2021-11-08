9f63c5e9b1dae2a161353774f981f998c9ae903e
lucasquearis <lucasquearis@hotmail.com>
2021-11-08 15:59:10 -0300

    requisito 2 feito

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
+const data = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -9,9 +11,19 @@ const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 app.get('/talker', (req, res) => {
-  const data = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
   if (!data) return res.status(HTTP_OK_STATUS).send([]);
   return res.status(HTTP_OK_STATUS).json(data);
+});
+
+app.get('/talker/:id', (req, res) => {
+  const { id: identificador } = req.params;
+  const filteredTalker = data.find(({ id }) => id === parseInt(identificador, 10));
+  if (!filteredTalker) {
+    return res
+    .status(404)
+    .send({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(HTTP_OK_STATUS).json(filteredTalker);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
