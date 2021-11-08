75e5d998f54e52a9bdc53d002a086fb565794786
lucasquearis <lucasquearis@hotmail.com>
2021-11-08 15:23:13 -0300

    requisito 1 feito

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', (req, res) => {
+  const data = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
+  if (!data) return res.status(HTTP_OK_STATUS).send([]);
+  return res.status(HTTP_OK_STATUS).json(data);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
